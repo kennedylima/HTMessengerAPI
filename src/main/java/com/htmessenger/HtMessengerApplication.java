1cda704f113311b5bc420cbeb6f0744c6235cd22
kennedyhightechcursos <kennedy@hightechcursos.com.br>
2017-10-14 16:35:10 -0400

    Ajustando aplicação para a integração com o Heroku

--- src/main/java/com/htmessenger/HtMessengerApplication.java
+++ src/main/java/com/htmessenger/HtMessengerApplication.java
@@ -2,9 +2,19 @@ package com.htmessenger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class HtMessengerApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String bemVindo() {
+		return "Bem-Vindo ao HT Messenger API";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HtMessengerApplication.class, args);
