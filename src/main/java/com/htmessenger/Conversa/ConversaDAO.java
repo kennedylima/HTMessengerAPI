b67e40980999d256807bf749ad20e5513eefac6e
kennedyhightechcursos <kennedy@hightechcursos.com.br>
2017-10-25 19:35:51 -0300

    Ajustando a rota que busca as conversas

--- src/main/java/com/htmessenger/Conversa/ConversaDAO.java
+++ src/main/java/com/htmessenger/Conversa/ConversaDAO.java
@@ -25,7 +25,7 @@ public class ConversaDAO  implements ConversaRepository{
          return entityManager.createQuery("" +
                  "FROM Conversa c " +
                  "WHERE c.usuarioOrigem.id ="+usuarioOrigem+" AND c.usuarioDestino="+usuarioDestino+" " +
-                 "OR c.usuarioDestino.id="+usuarioOrigem+"c.usuarioOrigem.id ="+usuarioDestino+"" +
+                 "OR c.usuarioDestino.id="+usuarioOrigem+"c.usuarioOrigem.id ="+usuarioDestino+" " +
                  "ORDER BY c.id" ).getResultList();
     }
 
